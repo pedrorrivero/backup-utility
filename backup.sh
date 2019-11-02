662e7e8f4ff27e51b21f16f3eebf0e65c90acdd2
Pedro Rivero <pedro.rivero.ramirez@gmail.com>
2019-11-02 16:40:15 -0500

    Added advenced functionality and formating

--- backup.sh
+++ backup.sh
@@ -3,18 +3,19 @@
 SOURCE_DIR='./test'
 BACKUP_DIR='./test.backup'
 
-LOG_FILE=$(date -u '+%Y-%m-%dT%H:%M:%SZ.log')
-tabs 4
+LOG_FILE="./logs/$(date -u '+%Y-%m-%dT%H:%M:%SZ.log')"
+
 
 ## ---- OPTION FLAGS ---- ##
 
 PARAMS=""
 OVERRIDE=""
 
-VERBOSE=''
+QUIET=''
 DRY_RUN=''
 NO_LOG=''
 WIPE=''
+FORCE=''
 
 
 ## ---- PARSER ---- ##
@@ -23,13 +24,14 @@ option_parser () {
 
   while (( "$#" )); do
   case "$1" in
-    -v|--verbose)
-      VERBOSE='set'
+    -q|--quiet)
+      QUIET='set'
       shift
       ;;
     -d|--dry-run)
       DRY_RUN='set'
-      VERBOSE='set'
+      NO_LOG='set'
+      echo -e "$(tput setab 7)$(tput setaf 0) DRY-RUN \n$(tput sgr 0)"
       shift
       ;;
     -n|--no-log)
@@ -42,23 +44,28 @@ option_parser () {
       ;;
     -w|--wipe-backup)
       WIPE='set'
-      shift 2
+      shift
+      ;;
+    -f|--force)
+      FORCE='set'
+      shift
       ;;
     -h|--help)
 
-      echo -en "\n"
       echo -e "BACKUP UTILITY:"\
-      "backup [options] SOURCE_DIR BACKUP_DIR"
-      echo -e "\t -v  --verbose: \t "\
-      "Log to stdout."
+      "backup [options] <SOURCE_DIR> <BACKUP_DIR> \n"
+      echo -e "\t -q  --quiet: \t "\
+      "Do not log to stdout."
       echo -e "\t -d  --dry-run: \t "\
       "Show changes to be made without actually running them."
       echo -e "\t -n  --no-log: \t "\
       "Do not create log file."
-      echo -e "\t -o  --override: \t "\
-      "[directory] Discard previous backup files for a given directory."
+      echo -e "\t -o  --override <subdirectory>: \t "\
+      "Discard previous backup files for a given subdirectory."
       echo -e "\t -w  --wipe-backup: \t "\
       "Wipe clean all previous backups before running."
+      echo -e "\t -f  --force: \t "\
+      "Do not prompt confirmation dialog."
       echo -e "\t -h  --help: \t "\
       "Display this help. "
       echo -en "\n"
@@ -91,13 +98,13 @@ global_to_array "OVERRIDE"
 ## ---- LOGS ---- ##
 
 
-subdirectory_log () {
+new_subdirectory_log () {
   # PARSING
   local backup=$1
   # FUNCTIONALITY
   local highlight="\t MAKING SUBDIRECTORY IN BACKUP: "
   local log=" $backup "
-  test $VERBOSE && echo -e "$(tput setaf 3)$highlight$(tput sgr 0)$log"
+  test ! $QUIET && echo -e "$(tput setaf 3)$highlight$(tput sgr 0)$log"
   test ! $NO_LOG && echo -e "$highlight$log" >> $LOG_FILE
 }
 
@@ -109,7 +116,7 @@ backup_log () {
   # FUNCTIONALITY
   local highlight=" BACKING-UP: "
   local log=" $SOURCE_DIR -> $BACKUP_DIR "
-  test $VERBOSE && echo -e "$(tput setaf 6)$highlight$(tput sgr 0)$log"
+  test ! $QUIET && echo -e "$(tput setaf 6)$highlight$(tput sgr 0)$log"
   test ! $NO_LOG && echo -e "$highlight$log" >> $LOG_FILE
 }
 
@@ -121,7 +128,7 @@ file_log () {
   # FUNCTIONALITY
   local highlight="\t CREATING FILE BACKUP: "
   local log=" $source -> $backup "
-  test $VERBOSE && echo -e "$(tput setaf 2)$highlight$(tput sgr 0)$log"
+  test ! $QUIET && echo -e "$(tput setaf 2)$highlight$(tput sgr 0)$log"
   test ! $NO_LOG && echo -e "$highlight$log" >> $LOG_FILE
 }
 
@@ -132,7 +139,7 @@ override_log () {
   # FUNCTIONALITY
   local highlight=" OVERRIDING: "
   local log=" $backup "
-  test $VERBOSE && echo -e "$(tput setaf 6)$highlight$(tput sgr 0)$log"
+  test ! $QUIET && echo -e "$(tput setaf 6)$highlight$(tput sgr 0)$log"
   test ! $NO_LOG && echo -e "$highlight$log" >> $LOG_FILE
 }
 
@@ -140,7 +147,7 @@ override_log () {
 wipe_log () {
   # FUNCTIONALITY
   local log=" WIPING OUT BACKUP "
-  test $VERBOSE && echo -e "$(tput setaf 6)$log$(tput sgr 0)"
+  test ! $QUIET && echo -e "$(tput setaf 6)$log$(tput sgr 0)"
   test ! $NO_LOG && echo -e $log >> $LOG_FILE
 }
 
@@ -157,6 +164,22 @@ warning_log () {
 
 
 ## ---- FUNCTIONS ---- ##
+
+
+init_backup_mode () {
+  tabs 4
+  echo -en "\n"
+}
+
+
+end_backup_mode () {
+  if [ -z $NO_LOG ]
+  then
+    echo -e "\n$(tput setab 0)$(tput setaf 7) Logs saved to: $(tput sgr 0) $LOG_FILE \n"
+  else
+    echo -en "\n"
+  fi
+}
 
 
 global_to_array () {
@@ -184,19 +207,27 @@ get_relative_path () {
 }
 
 
-override () {
-  local target=''
-  for subdirectory in "${OVERRIDE[@]}"
-  do
-    target="${BACKUP_DIR}/$(get_relative_path $subdirectory $SOURCE_DIR)"
-    if [ -d $target ]
-    then
-      override_log $target
-      rm -rf $target
-    else
-      warning_log $subdirectory
-    fi
-  done
+confirm_answer () {
+  # PARSING
+  local confirmation=$1
+  # FUNCTIONALITY
+  if [ $confirmation != 'y' ] && [ $confirmation != 'Y' ]
+  then
+    echo -e "Backup cancelled... \n"
+    exit 0
+  else
+    echo -en "\n"
+  fi
+}
+
+
+ask_confirmation () {
+  local message=$1
+  if [ -z $FORCE ]
+  then
+    read -p "Confirm $1 [y/n]: " confirmation
+    confirm_answer $confirmation
+  fi
 }
 
 
@@ -225,7 +256,7 @@ backup_if_new () {
   # FUNCTIONALITY
   if is_new_subdirectory $source $backup
   then
-    subdirectory_log $backup
+    new_subdirectory_log $backup
     test ! $DRY_RUN && mkdir $backup
   elif is_new_file $source $backup
   then
@@ -235,7 +266,7 @@ backup_if_new () {
 }
 
 
-update_backup_directory () {
+do_backup () {
   # PARSING
   local SOURCE_DIR=$1
   local BACKUP_DIR=$2
@@ -252,10 +283,62 @@ update_backup_directory () {
 }
 
 
+create_log_file () {
+  if [ -z $NO_LOG ]
+  then
+    mkdir ./logs 2> /dev/null
+    touch $LOG_FILE 2> /dev/null
+  fi
+}
+
+
+override_if_requested () {
+  # PARSING
+  local target=$1
+  # FUNCTIONALITY
+  if [ -z $DRY_RUN ]
+  then
+    ask_confirmation "subdirectory override"
+    rm -rf $target
+  fi
+}
+
+
+override_requested_subdirecrories () {
+  local target=''
+  for subdirectory in "${OVERRIDE[@]}"
+  do
+    target="${BACKUP_DIR}/$(get_relative_path $subdirectory $SOURCE_DIR)"
+    if [ -d $target ]
+    then
+      override_log $target
+      override_if_requested $target
+    else
+      warning_log $subdirectory
+    fi
+  done
+}
+
+
+wipe_backup_if_requested () {
+  # PARSING
+  local BACKUP_DIR=$1
+  # FUNCTIONALITY
+  if [ ! -z $WIPE ]
+  then
+    wipe_log
+    ask_confirmation "backup wipe"
+    rm -rf $BACKUP_DIR/*
+  fi
+}
+
+
 ## ---- CODE TO EXECUTE ---- ##
 
+init_backup_mode
 option_parser $@
-test ! $NO_LOG && touch $LOG_FILE 2> /dev/null
-override
-update_backup_directory $SOURCE_DIR $BACKUP_DIR
-test ! $NO_LOG && echo "Logs saved to: $LOG_FILE\n"
+create_log_file
+override_requested_subdirecrories
+wipe_backup_if_requested $BACKUP_DIR
+do_backup $SOURCE_DIR $BACKUP_DIR
+end_backup_mode
