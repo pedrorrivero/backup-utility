4f46647c8af17cad3fb868a682959b8d77c737a4
Pedro Rivero <pedro.rivero.ramirez@gmail.com>
2019-11-02 04:18:58 -0500

    Updated basic functionality

--- backup.sh
+++ backup.sh
@@ -3,27 +3,35 @@
 SOURCE_DIR='./test'
 BACKUP_DIR='./test.backup'
 
-source_tree=($(find ${SOURCE_DIR} | sort))
-backup_tree=($(find ${BACKUP_DIR} | sort))
+backup_new () {
+  local SOURCE_DIR=$1
+  local BACKUP_DIR=$2
 
-echo -e " BACKING-UP: $SOURCE_DIR -> $BACKUP_DIR"
+  local source_tree=($(find ${SOURCE_DIR} | sort))
+  local backup_tree=($(find ${BACKUP_DIR} | sort))
 
-for entry in "${source_tree[@]:1}"
-do
-  name=$(echo $entry | sed -e "s,^${SOURCE_DIR},," -e "s,^\/,,")
-  type=$(echo $entry | xargs file | sed -e "s,^${entry}\: ,,")
-  # echo "${name}: ${type}"
+  echo -e " BACKING-UP: $SOURCE_DIR -> $BACKUP_DIR"
 
-  source="${SOURCE_DIR}/${name}"
-  backup="${BACKUP_DIR}/${name}"
+  for entry in "${source_tree[@]:1}"
+  do
+    local path=$(echo $entry | sed -e "s,^${SOURCE_DIR},," -e "s,^\/,,")
+    local type=$(echo $entry | xargs file | sed -e "s,^${entry}\: ,,")
 
-  if [ $type == 'directory' ] && [ ! -d $backup ]
-  then
-    echo -e "\t MAKING DIRECTORY IN BACKUP: $backup"
-    mkdir $backup
-  elif [ $type != 'directory' ] && [ ! $backup -nt $source ]
-  then
-    echo -e "\t CREATING BACKUP: $source -> $backup"
-    cp $source $backup
-  fi
-done
+    local source="${SOURCE_DIR}/${path}"
+    local backup="${BACKUP_DIR}/${path}"
+
+    if [ $type == 'directory' ] && [ ! -d $backup ]
+    then
+      echo -e "\t MAKING DIRECTORY IN BACKUP: $backup"
+      mkdir $backup
+    elif [ $type != 'directory' ] && [ ! $backup -nt $source ]
+    then
+      echo -e "\t CREATING BACKUP: $source -> $backup"
+      cp $source $backup
+    fi
+  done
+}
+
+## CODE TO EXECUTE ##
+tabs 4
+backup_new $SOURCE_DIR $BACKUP_DIR
